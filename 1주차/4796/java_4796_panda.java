c73a960329aa7bef7be8a103eea12be66649237c
woong7 <39939886+woong7@users.noreply.github.com>
2022-06-28 23:42:27 +0900

    Update and rename java_17362_panda.java to java_4796_panda.java

---
+++ 1주차/4796/java_4796_panda.java
@@ -0,0 +1,32 @@
+package algorithm_study.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class java_4796_panda {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNumber = 0;
+        while (true) {
+            String nextLine = br.readLine();
+            if (nextLine.equals("0 0 0")) {
+                break;
+            }
+            caseNumber++;
+            StringTokenizer st = new StringTokenizer(nextLine);
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            int answer;
+            if (V % P >= L) {
+                answer = L * (V / P + 1);
+            } else {
+                answer = L * (V / P) + V % P;
+            }
+            System.out.printf("Case %d: %d\n", caseNumber, answer);
+        }
+    }
+}
