0316f4b4fff0083853c0ac9513bd1c5a9419ad7a
이프(sinb57) <sinb57@naver.com>
2022-06-29 19:22:33 +0900

    Update 4796_python_이프.py

--- 1주차/4796/4796_python_이프.py
+++ 1주차/4796/4796_python_이프.py
@@ -1,35 +1,22 @@
 
-
 def solution(L, P, V):
 
     if (P > V):
         return L
 
-    answer = L * (V // P)
-    remains = V % P
-
-    if (remains > L):
-        answer += L
-    else:
-        answer += remains
-        
+    answer = min(L, V % P)
+    answer += L * (V // P)
     return answer
-    
-
 
 prob_count = 0
 
 while True:
-
     text = input()
-
     if (text == '0 0 0'):
         break
 
-    prob_count += 1
-
     L, P, V = map(int, text.split())
-
     answer = solution(L, P, V)
 
+    prob_count += 1
     print("Case %d: %d" % (prob_count, answer))
