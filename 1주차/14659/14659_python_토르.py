33333ea12a9dc8660847a375b48df56d9630bad5
injoon2019 <ison@sfu.ca>
2022-06-30 19:38:19 +0900

    14659 성능 개선

--- 1주차/14659/14659_python_토르.py
+++ 1주차/14659/14659_python_토르.py
@@ -3,19 +3,18 @@ import sys
 N = int(input())
 hills = list(map(int, input().split()))
 
-answer = -sys.maxsize
-
-def count_right_max(start_index):
-    count = 0
-    for i in range(start_index + 1, len(hills)):
-        if hills[i] < hills[start_index]:
-            count += 1
-        else:
-            break
-    return count
+current_height = 0
+max_height = 0
+count = 0
+answer = 0
 
 for i in range(len(hills)):
-    right_max = count_right_max(i)
-    answer = max(answer, right_max)
+    if hills[i] > max_height:
+        count = 0
+        max_height = hills[i]
+    else:
+        count += 1
+
+    answer = max(answer, count)
 
 print(answer)
