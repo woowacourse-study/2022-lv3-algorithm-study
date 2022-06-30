bd57eb68d79448e46b5536730bc88d1000090cb1
이프(sinb57) <sinb57@naver.com>
2022-06-30 09:08:43 +0900

    Update 10814_python_이프.py

--- 1주차/10814/10814_python_이프.py
+++ 1주차/10814/10814_python_이프.py
@@ -1,4 +1,4 @@
-
+## 첫번째 풀이
 registers = []
 
 for _ in range(int(input())):
@@ -7,4 +7,21 @@ for _ in range(int(input())):
 
 for age, name in sorted(registers, key=lambda x:x[0]):
     print(age, name)
+
+
+#############################
+## 두번째 풀이 (첫번째 풀이에 비해 더 적은 시간복잡도)
+people = [[] for i in range(201)]
+
+for _ in range(int(input())):
+    age,name = input().split()
+    age = int(age)
     
+    people[age].append(name)
+    
+for age in range(1, 201):
+    names = people[age]
+    if (len(names) == 0):
+        continue
+    for name in names:
+        print(age,name)
