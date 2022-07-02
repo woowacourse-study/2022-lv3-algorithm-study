f54fb54a2fb2300a6ab7dac1a7fb917677fa42c7
summerlunaa <summerlunaa9@gmail.com>
2022-07-03 06:07:40 +0900

    리뷰 반영

--- 1주차/17362/17362_python_파랑.py
+++ 1주차/17362/17362_python_파랑.py
@@ -1,13 +1,39 @@
 import sys
 
-n = int(sys.stdin.readline())
 
-if n == 1:
-    print(n)
-else:
-    n -= 2
-    if n // 4 % 2 == 1:
-        result = 4 - (n % 4)
+def find_finger_number1(num):
+    if num == 1:
+        print(num)
     else:
-        result = (n % 4) + 2
+        num -= 2
+        if num // 4 % 2 == 1:
+            result = 4 - (num % 4)
+        else:
+            result = (num % 4) + 2
+        print(result)
+
+
+def find_finger_number2(num):
+    result = (num - 1) % 8 + 1
+
+    if result >= 5:
+        result = 10 - result
+
     print(result)
+
+
+def find_finger_number3(num):
+    remainder = num % 8
+
+    if remainder == 6:
+        print(4)
+    elif remainder == 7:
+        print(3)
+    elif remainder == 0:
+        print(2)
+    else:
+        print(remainder)
+
+
+n = int(sys.stdin.readline())
+find_finger_number2(n)
