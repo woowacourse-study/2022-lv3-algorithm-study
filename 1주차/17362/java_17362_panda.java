f07915db9c119009f644131ac9553e5a0cea7db5
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-04 01:11:29 +0900

    Update java_17362_panda.java

--- 1주차/17362/java_17362_panda.java
+++ 1주차/17362/java_17362_panda.java
@@ -8,25 +8,6 @@ public class java_17362_panda {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
-        System.out.println(findFinger(number));
-    }
-    private static int findFinger(int number){
-        switch ((number - 1) % 8) {
-            case 0:
-                return 1;
-            case 1:
-            case 7:
-                return 2;
-            case 2:
-            case 6:
-                return 3;
-            case 3:
-            case 5:
-                return 4;
-            case 4:
-                return 5;
-
-        }
-        return -1;
+        System.out.println(5 - Math.abs(4 - (number - 1) % 8));
     }
 }
