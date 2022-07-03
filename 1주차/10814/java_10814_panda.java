4441e8bbbaafc4623ac1090cc3f623429860c1e4
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-04 01:49:17 +0900

    Update java_10814_panda.java

--- 1주차/10814/java_10814_panda.java
+++ 1주차/10814/java_10814_panda.java
@@ -1,8 +1,10 @@
 package algorithm_study.week1;
 
-import java.io.*;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -11,42 +13,22 @@ public class java_10814_panda {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
 
-        List<Member> members = new LinkedList<>();
+        List<String>[] members = new List[201];
+        Arrays.setAll(members, element -> new ArrayList<>());
+
         for (int i = 0; i < number; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            members.add(new Member(age, name));
+            members[age].add(name);
         }
-        Collections.sort(members);
 
         StringBuilder sb = new StringBuilder();
-        for (Member member : members) {
-            sb.append(member.getAge()).append(" ").append(member.getName()).append("/");
+        for (int age = 1; age <= 200; age++) {
+            for (String name : members[age]) {
+                sb.append(age).append(" ").append(name).append("\n");
+            }
         }
         System.out.println(sb);
-    }
-}
-
-class Member implements Comparable<Member> {
-    private final int age;
-    private final String name;
-
-    public Member(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int compareTo(Member o) {
-        return age - o.age;
     }
 }
