654026a9456f323d74e677f451e93e756614e391
sinb57 <sinb57@naver.com>
2022-07-05 03:11:14 +0900

    Create 23827_python_이프.py

---
+++ 2주차/23827/23827_python_이프.py
@@ -0,0 +1,24 @@
+limit = 1000000007
+
+size = int(input())
+
+numbers = []
+total = 0
+for i in input().split():
+    value = int(i)
+    numbers.append(value)
+    total += value
+    total %= limit
+
+answer = 0
+for i in range(size-1):
+    current = numbers[i]
+
+    total -= current
+    if (total < 0):
+        total += limit
+
+    answer += current * total
+    answer %= limit
+
+print(answer)
