26ac6a076c4bf2bbb94e61c957c3205094268ac3
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2022-07-05 21:37:35 +0900

    Update 11866_java_라쿤.java
    잘못..올림..ㅎ

--- 2주차/11866/11866_java_라쿤.java
+++ 2주차/11866/11866_java_라쿤.java
@@ -1,82 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
 
-    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static String input() {
         try {
-            return reader.readLine().trim();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            return br.readLine().trim();
+        } catch (Exception e) {}
+        return null;
     }
 
     public static void main(String[] args) {
         String[] datas = input().split(" ");
-        BigInteger heroHp = BigInteger.valueOf(Long.parseLong(datas[0]));
-        BigInteger heroAtk = BigInteger.valueOf(Long.parseLong(datas[1]));
-        BigInteger devilHp = BigInteger.valueOf(Long.parseLong(datas[2]));
-        BigInteger devilAtk = BigInteger.valueOf(Long.parseLong(datas[3]));
+        int n = Integer.parseInt(datas[0]);
+        int k = Integer.parseInt(datas[1]);
 
-        datas = input().split(" ");
-        BigInteger devilSkillMoment = BigInteger.valueOf(Long.parseLong(datas[0]));
-        BigInteger devilSkillEffect = BigInteger.valueOf(Long.parseLong(datas[1]));
-
-        // devilSkillMoment까지의 공격횟수
-        BigInteger midAtkCount = devilHp.subtract(devilSkillMoment).divide(heroAtk);
-        if (devilHp.subtract(devilSkillMoment).mod(heroAtk).intValue() != 0) {
-            midAtkCount = midAtkCount.add(BigInteger.ONE);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < n+1; i++) {
+            queue.offer(i);
         }
 
-        BigInteger devilMidAtk = devilAtk.multiply(midAtkCount);
-        BigInteger destHeroHp = heroHp.subtract(devilMidAtk);
-
-        // devil의 체력이 devilSkillMoment 이하가 되는 순간 hero와 devil의 체력
-        BigInteger heroMidAtk = heroAtk.multiply(midAtkCount);
-        BigInteger destDevilHp = devilHp.subtract(heroMidAtk);
-
-        if (destHeroHp.add(devilAtk).compareTo(BigInteger.ZERO) >= 0 && destDevilHp.compareTo(BigInteger.ZERO) <= 0) {
-            System.out.println("Victory!");
-            return;
-        }
-
-        // 처음부터 devilHp에 스킬 효과 적용
-        devilHp = devilHp.add(devilSkillEffect);
-
-        // 이분 탐색 진행
-        long left = 0;
-        long right = 100000000000L;
-        long mid = 0;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            BigInteger devilDamage = devilAtk.multiply(BigInteger.valueOf(mid));
-            BigInteger heroDamage = heroAtk.multiply(BigInteger.valueOf(mid));
-
-            BigInteger leftHeroHp = heroHp.subtract(devilDamage);
-            BigInteger leftDevilHp = devilHp.subtract(heroDamage);
-
-            if (leftHeroHp.compareTo(BigInteger.ZERO) <= 0 && leftDevilHp.compareTo(BigInteger.ZERO) > 0) {
-                System.out.println("gg");
-                return;
+        StringBuffer sb = new StringBuffer();
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < k-1; i++) {
+                queue.offer(queue.poll());
             }
-            if (leftHeroHp.compareTo(devilAtk.negate()) > 0 && leftDevilHp.compareTo(BigInteger.ZERO) <= 0) {
-                System.out.println("Victory!");
-                return;
-            }
-
-            if (leftHeroHp.compareTo(BigInteger.ZERO) <= 0 && leftDevilHp.compareTo(BigInteger.ZERO) <= 0) {
-                right = mid - 1;
-            }
-
-            if (leftHeroHp.compareTo(BigInteger.ZERO) > 0 && leftDevilHp.compareTo(BigInteger.ZERO) > 0) {
-                left = mid + 1;
+            sb.append(queue.poll());
+            if (!queue.isEmpty()) {
+                sb.append(", ");
             }
         }
+
+        String result = String.join(", ", sb.toString());
+        result = "<" + result + ">";
+
+        System.out.println(result);
     }
-
-
 }
