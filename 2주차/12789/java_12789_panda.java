5a34ed496f97f0c8a3a930717391cba333c3c9d9
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-08 00:02:17 +0900

    Update java_12789_panda.java

--- 2주차/12789/java_12789_panda.java
+++ 2주차/12789/java_12789_panda.java
@@ -1,8 +1,7 @@
-package algorithm_study.week2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class java_12789_panda {
@@ -10,25 +9,18 @@ public class java_12789_panda {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] waitingLine = new int[N];
-        int pivot = -1;
+        Stack<Integer> waitingLine = new Stack<>();
+        waitingLine.push(N+2);
         int current = 1;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            int top = Integer.parseInt(st.nextToken());
-            if (top == current) {
+            waitingLine.push(Integer.parseInt(st.nextToken()));
+            while (waitingLine.peek() == current) {
+                waitingLine.pop();
                 current++;
-                while (pivot >= 0 && waitingLine[pivot] == current) {
-                    pivot--;
-                    current++;
-                }
-            } else {
-                waitingLine[++pivot] = top;
             }
         }
-
-        String answer = pivot < 0 ? "Nice" : "Sad";
-        System.out.println(answer);
+        System.out.println(current > N ? "Nice" : "Sad");
     }
 }
