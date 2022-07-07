ac4f9027ba1f5bb70f1d64c209fa40645bc26ba4
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-07 13:40:44 +0900

    Create java_2121_panda.java

---
+++ 2주차/2121/java_2121_panda.java
@@ -0,0 +1,50 @@
+package algorithm_study.week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class java_2121_panda {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        Map<Integer, List<Integer>> points = new HashMap<>();
+        for (int i = 0; i <= A; i++) {
+            points.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            points.computeIfAbsent(x, k -> new ArrayList<>());
+            points.get(x).add(y);
+        }
+
+        int answer = 0;
+
+        for (int x : points.keySet()) {
+            if (!points.containsKey(x + A)) {
+                continue;
+            }
+            for (int y : points.get(x)) {
+                if (points.get(x).contains(y + B)
+                    && points.get(x + A).containsAll(List.of(y, y + B))) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
