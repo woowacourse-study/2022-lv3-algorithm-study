1cdca272a2674798bebd3d3b1d5ac59b11ba63c6
hyunrrr <whgusrms96@gmail.com>
2022-07-11 15:49:01 +0900

    11866번 다시품

--- 2주차/11866/josh_java_11866.java
+++ 2주차/11866/josh_java_11866.java
@@ -2,42 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class josh_java_11866 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-        int num = Integer.parseInt(inputs[0]);
-        int gap = Integer.parseInt(inputs[1]);
-        List<Boolean> bucket = new ArrayList<>(Collections.nCopies(num, true));
-        List<Integer> answer = new ArrayList<>();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int index = -1;
-        while (answer.size() != num) {
-            index = findTarget(bucket, index, gap);
-            answer.add(index + 1);
-        }
+        int maxNum = Integer.parseInt(st.nextToken());
+        int moveNum = Integer.parseInt(st.nextToken());
+
+        List<Integer> bucket = makeBucket(maxNum);
+        List<Integer> answer = josephusResult(bucket, moveNum);
 
         String output = answer.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", "));
 
-        System.out.println("<" + output + ">");
+        sb.append("<");
+        sb.append(output);
+        sb.append(">");
+
+        System.out.println(sb);
+        bf.close();
     }
 
-    private static int findTarget(List<Boolean> bucket, int index, int gap) {
-        int limit = bucket.size();
-        while (gap != 0) {
-            index = (index + 1) % limit;
-            if (bucket.get(index)) {
-                gap -= 1;
-            }
+    private static List<Integer> josephusResult(List<Integer> bucket, int moveNum) {
+        List<Integer> result = new ArrayList<>();
+
+        int index = 0;
+        while (!bucket.isEmpty()) {
+            index = getIndex(index, moveNum, bucket.size());
+            result.add(bucket.get(index));
+            bucket.remove(index);
         }
-        bucket.set(index, false);
-        return index;
+        return result;
+    }
+
+    private static int getIndex(int index, int moveNum, int upperLimit) {
+        int result = index + moveNum - 1;
+        if (result >= upperLimit) {
+            result %= upperLimit;
+        }
+        return result;
+    }
+
+    private static List<Integer> makeBucket(int maxNum) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < maxNum; ++i) {
+            result.add(i + 1);
+        }
+        return result;
     }
 }
+
