a3e405d83a8ed2d94aa1698f99a6b6a4c591e145
Wishoon <heonga2@gmail.com>
2022-07-16 21:10:37 +0900

    fix: import 추가

--- 3주차/11568/11568_java_루키.java
+++ 3주차/11568/11568_java_루키.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 11568_java_루키 {
 
     public static void main(String[] args) throws IOException {
--- 3주차/1254/1254_java_루키.java
+++ 3주차/1254/1254_java_루키.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 1254_java_루키 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
