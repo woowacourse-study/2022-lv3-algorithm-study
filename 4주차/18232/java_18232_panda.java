7682403195bcc6ec2b998e9f1c69b71a6f78ba19
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-19 02:34:56 +0900

    Create java_18232_panda.java

---
+++ 4주차/18232/java_18232_panda.java
@@ -0,0 +1,69 @@
+package algorithm_study.week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class java_18232_panda {
+    static int N, M, S, E;
+    static Map<Integer, Set<Integer>> teleport;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        teleport = new HashMap<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int firstSpot = Integer.parseInt(st.nextToken());
+            int secondSpot = Integer.parseInt(st.nextToken());
+            teleport.computeIfAbsent(firstSpot, k -> new HashSet<>()).add(secondSpot);
+            teleport.computeIfAbsent(secondSpot, k -> new HashSet<>()).add(firstSpot);
+        }
+        System.out.println(findMinTime());
+    }
+
+    private static int findMinTime() {
+        visited[S] = true;
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.add(List.of(S, 0));
+        while (!queue.isEmpty()) {
+            List<Integer> current = queue.poll();
+            int departure = current.get(0);
+            int timeSpent = current.get(1) + 1;
+
+            Set<Integer> nextSpots = teleport.computeIfAbsent(departure, k -> new HashSet<>());
+            Stream.of(departure - 1, departure + 1)
+                .filter(x -> x > 0 && x <= N)
+                .forEach(nextSpots::add);
+
+            for (int next : nextSpots) {
+                if (visited[next])
+                    continue;
+                if (next == E) {
+                    return timeSpent;
+                }
+                visited[next] = true;
+                queue.add(List.of(next, timeSpent));
+            }
+        }
+        return 0;
+    }
+}
