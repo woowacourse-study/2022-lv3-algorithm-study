ecc5dcbc7f3c08c68aee19a29a9e60821e29309b
sinb57 <sinb57@naver.com>
2022-07-18 20:36:11 +0900

    Update 17123_python_이프.py

--- 3주차/17123/17123_python_이프.py
+++ 3주차/17123/17123_python_이프.py
@@ -6,29 +6,22 @@ IDX = -1
 for _ in range(int(input())):
     size, times = map(int, input().split())
 
-    results = dict()
-    for i in range(size):
-        results[(i, IDX)] = 0
-        results[(IDX, i)] = 0
+    rows = [0] * size
+    cols = [0] * size
 
     for row in range(size):
         items = input().split()
         for col in range(size):
             item = int(items[col])
-            results[(row, IDX)] += item
-            results[(IDX, col)] += item
+            rows[row] += item
+            cols[col] += item
 
     for __ in range(times):
-        r1, c1, r2, c2, v = map(int,input().split())
+        r1, c1, r2, c2, v = map(int, input().split())
         for i in range(r1 - 1, r2):
-            results[(i, IDX)] += v * (c2 - c1 + 1)
+            rows[i] += v * (c2 - c1 + 1)
         for i in range(c1 - 1, c2):
-            results[(IDX, i)] += v * (r2 - r1 + 1)
+            cols[i] += v * (r2 - r1 + 1)
 
-    answers = ['', '']
-    for i in range(size):
-        answers[0] += str(results[(i, IDX)]) + ' '
-        answers[1] += str(results[(IDX, i)]) + ' '
-
-    for answer in answers:
-        print(answer[:-1])
+    print(*rows)
+    print(*cols)
