3ecb72f22b8f84bcfa2979f30e967907874039ef
DWL21 <nggus5@naver.com>
2022-07-23 17:45:10 +0900

    4주차 21938 제출

---
+++ 4주차/21938/21938-python-leo.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def pop(i, j):
+    if i < 0 or j < 0 or i >= M or j >= N:
+        return
+
+    if new_display[i][j]:
+        new_display[i][j] = False
+        pop(i + 1, j)
+        pop(i - 1, j)
+        pop(i, j + 1)
+        pop(i, j - 1)
+
+
+def solution(T, display):
+    T = T * 3
+    for j in range(N):
+        for i in range(M):
+            index = i * 3
+            if display[j][index] + display[j][index + 1] + display[j][index + 2] >= T:
+                new_display[j].append(True)
+            else:
+                new_display[j].append(False)
+    answer = 0
+    for j in range(N):
+        for i in range(M):
+            if new_display[i][j]:
+                answer += 1
+                pop(i, j)
+    return answer
+
+
+M, N = map(int, input().split())
+new_display = [[] for _ in range(N)]
+display = []
+for _ in range(N):
+    display.append(list(map(int, input().split())))
+T = int(input())
+print(solution(T, display))
