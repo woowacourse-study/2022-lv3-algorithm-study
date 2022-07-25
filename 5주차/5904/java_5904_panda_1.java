15a67357dddbdeffcb0d033b7eda5118a57022be
woong7 <39939886+woong7@users.noreply.github.com>
2022-07-26 03:07:20 +0900

    Update java_5904_panda_1.java

--- 5주차/5904/java_5904_panda_1.java
+++ 5주차/5904/java_5904_panda_1.java
@@ -1,8 +1,6 @@
 package algorithm_study.week5;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 public class java_5904_panda {
