152e3c2a7a4a621ea7c68619c052ce62fdd165b5
yxxnghwan <younghwan960@gmail.com>
2022-07-31 15:16:58 +0900

    [알렉스] 무게임 정복

--- 5주차/5904/Java_5904_이영환.java
+++ 5주차/5904/Java_5904_이영환.java
@@ -8,18 +8,62 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final int n = Integer.parseInt(bf.readLine());
-        final char character = findThisPositionCharacter("moo", n, 1);
 
+        final Cycle cycle = findCycleNumber(n);
+
+        final String character = moo(cycle, n);
         System.out.println(character);
     }
 
-    private static char findThisPositionCharacter(final String moo, final int n, final int cycle) {
-        if (moo.length() < n) {
-            final String newMoo = moo + "m"
-                    + "o".repeat(cycle + 2)
-                    + moo;
-            return findThisPositionCharacter(newMoo, n, cycle + 1);
+    private static String moo(final Cycle cycle, final int n) {
+        if (n == 1) {
+            return "m";
         }
-        return moo.charAt(n - 1);
+
+        if (n <= cycle.calculateBeforeSize()) {
+            return moo(new Cycle(cycle.cycle - 1, cycle.calculateBeforeSize()), n);
+        } else if (n <= cycle.calculateBeforeSize() + cycle.calculateMiddleLength()) {
+            if (n - cycle.calculateBeforeSize() == 1) {
+                return "m";
+            } else {
+                return "o";
+            }
+        } else {
+            return moo(
+                    new Cycle(cycle.cycle - 1, cycle.calculateBeforeSize()),
+                    n - (cycle.calculateMiddleLength() + cycle.calculateBeforeSize())
+            );
+        }
+    }
+
+    private static Cycle findCycleNumber(final int n) {
+        int cycle = 0;
+        int size = 3;
+
+        while (size < n) {
+            cycle++;
+            size = size + (cycle + 3) + size;
+        }
+
+        return new Cycle(cycle, size);
+    }
+
+    static class Cycle {
+
+        final int cycle;
+        final int size;
+
+        public Cycle(final int cycle, final int size) {
+            this.cycle = cycle;
+            this.size = size;
+        }
+
+        public int calculateBeforeSize() {
+            return (size - (cycle + 3)) / 2;
+        }
+
+        public int calculateMiddleLength() {
+            return cycle + 3;
+        }
     }
 }
