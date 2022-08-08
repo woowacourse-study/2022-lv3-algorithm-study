68f3dcdc90ee9a0d807963420e310f9a5615f347
Hongdonggeon <67885363+Hongdonggeon@users.noreply.github.com>
2022-08-09 00:17:32 +0900

    [쿤] 6주차 과제 제출 (#205)
    * Kun_1254
    
    * kun_16198
    
    * 불필요한 파일 ㄱ삭제
    
    * 불필요한 파일 삭제
    
    * 불필요한 파일 삭제
    
    * kun_5107
    
    * kun_16198
    
    * 불필요한 파일 ㄱ삭제
    
    * 불필요한 파일 삭제
    
    * 불필요한 파일 삭제
    
    * kun_5107
    
    * kun_24393
    
    * kun_14938

---
+++ 6주차/24393/Kun_24393.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Kun_24393 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int index = 1;
+        boolean isJokerLeft;
+        for (int i = 0; i < N; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int stackedNumber = 0;
+            boolean isLeft = false;
+            if (index <= 13) {
+                isJokerLeft = true;
+            } else {
+                isJokerLeft = false;
+                index -= 13;
+            }
+
+            while (st.hasMoreTokens()) {
+                int currentCardsNumber = Integer.parseInt(st.nextToken());
+                if (isLeft == isJokerLeft) {
+                    if (currentCardsNumber < index) {
+                        stackedNumber += currentCardsNumber;
+                        index -= currentCardsNumber;
+                    } else {
+                        index += stackedNumber;
+                        while (st.hasMoreTokens()) {
+                            st.nextToken();
+                        }
+                        break;
+                    }
+                } else {
+                    stackedNumber += currentCardsNumber;
+                }
+                isLeft = !isLeft;
+            }
+        }
+        System.out.println(index);
+    }
+}
